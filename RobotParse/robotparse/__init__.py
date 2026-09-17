ee19393d0076a85a643a4842f34577738bd9e01a
cmroper29 <63426624+cmroper29@users.noreply.github.com>
2026-09-17 08:29:05 -0600

    Delete RobotParse directory

--- RobotParse/robotparse/__init__.py
+++
@@ -1,6 +0,0 @@
-"""robotparse: robot programs (KUKA KRL, FANUC TP, ABB RAPID) to sampled TCP trajectories."""
-from .config import load_config
-from .parsers import detect_vendor, parse_program
-
-__all__ = ["load_config", "parse_program", "detect_vendor"]
-__version__ = "0.1.0"
